19c3e270f27f0db71f1b1f0ae020540061baca04
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-15 18:34:46 +0600

    added new font family

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -20,37 +20,37 @@ const Navbar: React.FC = () => {
     <div className="mx-auto transition-all bg-white/60 w-full h-20 relative top-0 border-b border-gray-200 z-50 ">
       <nav className="relative flex justify-between items-center px-4 md:px-12 py-4 max-w-7xl bg-white/60 w-full h-20 m-auto lg:py-2.5">
         <Link href={'/'} className="grid gap-1">
-          <h3 className="text-lg">teenage engineering</h3>
+          <h3 className="text-lg font-extralight">teenage engineering</h3>
           <span className="text-gray-400">
             {new Date().getFullYear()} {months[new Date().getMonth()]}
           </span>
         </Link>
         <Link href={'/'} className="grid gap-1">
-          <h3 className="text-lg">teenage engineering</h3>
+          <h3 className="text-lg font-extralight">teenage engineering</h3>
           <span>
             {new Date().getFullYear()} {months[new Date().getMonth()]}
           </span>
         </Link>
         <Link href={'/'} className="grid gap-1">
-          <h3 className="text-lg">teenage engineering</h3>
+          <h3 className="text-lg font-extralight">teenage engineering</h3>
           <span>
             {new Date().getFullYear()} {months[new Date().getMonth()]}
           </span>
         </Link>
         <Link href={'/'} className="grid gap-1">
-          <h3 className="text-lg">teenage engineering</h3>
+          <h3 className="text-lg font-extralight">teenage engineering</h3>
           <span>
             {new Date().getFullYear()} {months[new Date().getMonth()]}
           </span>
         </Link>
         <Link href={'/'} className="grid gap-1">
-          <h3 className="text-lg">teenage engineering</h3>
+          <h3 className="text-lg font-extralight">teenage engineering</h3>
           <span>
             {new Date().getFullYear()} {months[new Date().getMonth()]}
           </span>
         </Link>
         <Link href={'/'} className="grid gap-1">
-          <h3 className="text-lg">teenage engineering</h3>
+          <h3 className="text-lg font-extralight">teenage engineering</h3>
           <span>
             {new Date().getFullYear()} {months[new Date().getMonth()]}
           </span>
