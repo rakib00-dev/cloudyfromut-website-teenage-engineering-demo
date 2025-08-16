59e50b1d68aa1c225b4d0706f61ee5fa077b60a0
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-16 20:54:16 +0600

    fixed the layout issues

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,11 +2,5 @@ import Footer from '@/components/custom/Footer';
 import Navbar from '@/components/custom/Navbar';
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Footer />
-      {/* <div className="mt-[80rem]"></div> */}
-    </>
-  );
+  return <>{/* <div className="mt-[80rem]"></div> */}</>;
 }
