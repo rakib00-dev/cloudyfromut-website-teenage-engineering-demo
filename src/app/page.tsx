f536a27f7e754219a1c49deded023c925fa952fa
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-16 20:32:41 +0600

    updated the layout and pages tree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Footer from '@/components/custom/Footer';
 import Hero from '@/components/custom/Hero';
+import Navbar from '@/components/custom/Navbar';
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 }
