5f8eec46adb8a29694e65f10b65ccc2ec3c38c4f
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-16 20:21:00 +0600

    added products page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/custom/Footer';
+import Navbar from '@/components/custom/Navbar';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+      {/* <div className="mt-[80rem]"></div> */}
+    </>
+  );
+}
