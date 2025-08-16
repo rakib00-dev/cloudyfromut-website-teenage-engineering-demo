72b716ac382520e89ef4731a5dcb481bc54959cd
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-16 20:31:22 +0600

    added productsNavbar

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/components/custom/Footer';
 import Navbar from '@/components/custom/Navbar';
-import Image from 'next/image';
 
 export default function Home() {
   return (
---
+++ src/components/custom/ProductsNavbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductsNavbar = () => {
+  return <div>ProductsNavbar</div>;
+};
+
+export default ProductsNavbar;
